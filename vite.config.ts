b4b9cb3cf8be54dd9938c50eb443f590fafb5460
jishnuvadakara <jishnuv750@gmail.com>
2024-10-30 19:34:53 +0530

    json file added

--- vite.config.ts
+++ vite.config.ts
@@ -5,3 +5,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import path from "path";
+// import { config } from "dotenv";
+// config();
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   define: {
+//     "process.env": process.env,
+//   },
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// });
