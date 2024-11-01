ecc670ca19876d0632ea57e29ee33ee8c1bf8306
jishnuvadakara <jishnuv750@gmail.com>
2024-11-01 19:51:46 +0530

    change the url

--- src/utils/axios/index.tsx
+++ src/utils/axios/index.tsx
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
-export const BASE_URL = "http://localhost:3001";
+// export const BASE_URL = "http://localhost:3001";
+export const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 console.log("🚀 ~ file: index.tsx:3 ~ BASE_URL:", BASE_URL);
 
 export const CLIENT_API = axios.create({
